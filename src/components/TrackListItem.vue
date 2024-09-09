973de7111b42917f24ddd2290b14c9a07787d1d9
云 <i@muir.fun>
2024-09-10 00:29:41 +0800

    Update TrackListItem.vue

--- src/components/TrackListItem.vue
+++ src/components/TrackListItem.vue
@@ -42,8 +42,11 @@
               :exclude="$parent.albumObject.artist.name"
               prefix="-"
           /></span>
-          <span v-if="isAlbum && (track.mark & 1048576) === 1048576" class="explicit-symbol"
-            ><ExplicitSymbol
+          <span
+            v-if="isAlbum && (track.mark & 1048576) === 1048576"
+            class="explicit-symbol"
+          >
+            <ExplicitSymbol
           /></span>
         </div>
         <div v-if="!isAlbum" class="artist">
